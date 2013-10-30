c4f98eda7ee126b7bf5a6ce5749ec8fa82411b82
René Albers <alre1011(at)hs-karlsruhe.de>
2013-10-30 13:05:46 +0100

    Änderungen in ArtikelResource

--- shop/src/main/java/Artikelverwaltung/rest/ArtikelResource.java
+++ shop/src/main/java/Artikelverwaltung/rest/ArtikelResource.java
@@ -1,11 +1,12 @@
 package Artikelverwaltung.rest;
 
-import static de.shop.util.Constants.SELF_LINK;
+import static util.Constants.SELF_LINK;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.MediaType.APPLICATION_XML;
 import static javax.ws.rs.core.MediaType.TEXT_XML;
 
 import java.net.URI;
+import util.rest.UriHelper;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
@@ -19,6 +20,8 @@ import javax.ws.rs.core.Link;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
+import util.Mock;
+import util.rest.UriHelper;
 import Artikelverwaltung.domain.Artikel;
 
 
@@ -40,7 +43,8 @@ import Artikelverwaltung.domain.Artikel;
 		@Path("{id:[1-9][0-9]*}")
 		public Response findArtikelById(@PathParam("id") Long id) {
 			// TODO Anwendungskern statt Mock, Verwendung von Locale
-			final Artikel artikel = Mock.findBestellungById(id);
+			// TODO findartikelbyid in mock hinzufügen 
+			final Artikel artikel = Mock.findArtikelById(id);
 			if (artikel == null) {
 				throw new NotFoundException("Kein Artikel mit der ID " + id + " gefunden.");
 			}
