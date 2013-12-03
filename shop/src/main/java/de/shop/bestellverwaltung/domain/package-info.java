890ae77544917a2c7086020cdb0e441668af3aa5
Matthias Brand <brma1028 (at) hs-karlsruhe.de>
2013-12-03 15:26:18 +0100

    package-info.java Dateien angelegt

---
+++ shop/src/main/java/de/shop/bestellverwaltung/domain/package-info.java
@@ -0,0 +1,18 @@
+@XmlAccessorType(FIELD)
+
+package de.shop.bestellverwaltung.domain;
+
+import static javax.xml.bind.annotation.XmlAccessType.FIELD;
+
+import javax.xml.bind.annotation.XmlAccessorType;
+
+
+
+
+/**
+ * 
+ */
+/**
+ * @author matze
+ *
+ */
