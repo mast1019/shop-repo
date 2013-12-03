46f0855fa32f5991ae9a0551cd150c4b323ec36b
René Albers <alre1011(at)hs-karlsruhe.de>
2013-12-03 12:30:44 +0100

    Korrektur in Posten

--- shop/src/main/java/de/shop/bestellverwaltung/domain/Posten.java
+++ shop/src/main/java/de/shop/bestellverwaltung/domain/Posten.java
@@ -13,7 +13,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+
 
 @XmlRootElement
 public class Posten implements Serializable {
