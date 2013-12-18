2a77c5afe301e3741e3cdfa18f1a372aa0fc1ddb
Matthias Brand <brma1028 (at) hs-karlsruhe.de>
2013-12-18 20:40:37 +0100

    JPA-Annotations und NamedQueries in Posten.java eingefügt

--- shop/src/main/java/de/shop/bestellverwaltung/domain/Posten.java
+++ shop/src/main/java/de/shop/bestellverwaltung/domain/Posten.java
@@ -1,5 +1,6 @@
 package de.shop.bestellverwaltung.domain;
 
+import java.lang.invoke.MethodHandles;
 import java.math.BigDecimal;
 import java.io.Serializable;
 import java.net.URI;
@@ -8,24 +9,59 @@ import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
 import de.shop.artikelverwaltung.domain.Artikel;
+import static de.shop.util.Constants.KEINE_ID;
 
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.PostPersist;
+import javax.persistence.Table;
 import javax.validation.Valid;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
+import org.jboss.logging.Logger;
 
+
+@Entity
+@Table(indexes =  {
+	@Index(columnList = "bestellung_fk"),
+	@Index(columnList = "artikel_fk")
+})
+@NamedQueries({
+ @NamedQuery(name  = Posten.FIND_LADENHUETER,
+	            query = "SELECT a"
+	            	    + " FROM   Artikel a"
+	            	    + " WHERE  a NOT IN (SELECT bp.artikel FROM Bestellposition bp)")
+})
 @XmlRootElement
 public class Posten implements Serializable {
 	private static final long serialVersionUID = -6546634107480260335L;
-	public static final int ANZAHL_MIN = 1;
+	private static final Logger LOGGER = Logger.getLogger(MethodHandles.lookup().lookupClass());
 	
-	private Long id;
+	private static final String PREFIX = "Bestellposition.";
+	public static final String FIND_LADENHUETER = PREFIX + "findLadenhueter";
 	
+	@Id
+	@GeneratedValue
+	@Basic(optional = false)
+	private Long id = KEINE_ID;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "artikel_fk", nullable = false)
+	@XmlTransient
 	@Valid
 	@NotNull(message = "{posten.artikel.notNull}")
 	private Artikel artikel;
 	
+	@Basic(optional = false)
 	@NotNull(message = "{posten.anzahl.notNull}")
 	@Min(value = 1, message = "{posten.anzahl.min}")	
 	private Integer anzahl;
@@ -37,14 +73,19 @@ public class Posten implements Serializable {
 	@XmlTransient
 	private URI artikelUri;
 
+	public Posten() {	
+	}
+	
 	public Posten(Long id, Artikel artikel, Integer anzahl) {
 		super();
 		this.id = id;
 		this.artikel = artikel;
 		this.anzahl = anzahl;
 	}
-
-	public Posten() {	
+	
+	@PostPersist
+	private void postPersist() {
+		LOGGER.debugf("Neue Bestellposition mit ID=%d", id);
 	}
 	
 	public Long getId() {
@@ -94,6 +135,54 @@ public class Posten implements Serializable {
 	public String toString() {
 		return "Posten [id=" + id + ", artikel=" + artikel.toString() + ", anzahl="
 				+ anzahl + ", gesamtpreis=" + gesamtpreis + "]";
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + anzahl;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((artikel == null) ? 0 : artikel.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final Posten other = (Posten) obj;
+		
+		// Bei persistenten Bestellpositionen koennen zu verschiedenen Bestellungen gehoeren
+		// und deshalb den gleichen Artikel (s.u.) referenzieren, deshalb wird Id hier beruecksichtigt
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		}
+		else if (!id.equals(other.id)) {
+			return false;
+		}
+
+		// Wenn eine neue Bestellung angelegt wird, dann wird jeder zu bestellende Artikel
+		// genau 1x referenziert (nicht zu verwechseln mit der "anzahl")
+		if (artikel == null) {
+			if (other.artikel != null) {
+				return false;
+			}
+		}
+		else if (!artikel.equals(other.artikel)) {
+			return false;
+		}
+		
+		return true;
 	}
 		
 }
