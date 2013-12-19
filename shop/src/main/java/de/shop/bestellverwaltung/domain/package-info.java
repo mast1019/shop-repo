55ea9e87d302ff35a27d035f2994a479538c2e85
Matthias Brand <brma1028 (at) hs-karlsruhe.de>
2013-12-19 11:16:15 +0100

    BestellungResource angepasst Methode in ArtikelService eingefügt

--- shop/src/main/java/de/shop/bestellverwaltung/domain/package-info.java
+++ shop/src/main/java/de/shop/bestellverwaltung/domain/package-info.java
@@ -1,18 +1,8 @@
 @XmlAccessorType(FIELD)
-
+@Vetoed
 package de.shop.bestellverwaltung.domain;
 
 import static javax.xml.bind.annotation.XmlAccessType.FIELD;
 
+import javax.enterprise.inject.Vetoed;
 import javax.xml.bind.annotation.XmlAccessorType;
-
-
-
-
-/**
- * 
- */
-/**
- * @author matze
- *
- */
--- shop/src/main/java/de/shop/bestellverwaltung/rest/BestellungResource.java
+++ shop/src/main/java/de/shop/bestellverwaltung/rest/BestellungResource.java
@@ -1,20 +1,29 @@
 package de.shop.bestellverwaltung.rest;
 
-import static de.shop.util.Constants.SELF_LINK;
+import static de.shop.bestellverwaltung.service.BestellungService.FetchType.NUR_BESTELLUNG;
 import static de.shop.util.Constants.ADD_LINK;
+import static de.shop.util.Constants.SELF_LINK;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.MediaType.APPLICATION_XML;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 import static javax.ws.rs.core.MediaType.TEXT_XML;
+import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-import javax.validation.Valid;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-//import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -23,17 +32,26 @@ import javax.ws.rs.core.Link;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
+import de.shop.artikelverwaltung.domain.Artikel;
+import de.shop.artikelverwaltung.rest.ArtikelResource;
+import de.shop.artikelverwaltung.service.ArtikelService;
+import de.shop.bestellverwaltung.domain.Posten;
 import de.shop.bestellverwaltung.domain.Bestellung;
+import de.shop.bestellverwaltung.service.BestellungService;
 import de.shop.kundenverwaltung.domain.AbstractKunde;
 import de.shop.kundenverwaltung.rest.KundeResource;
-import de.shop.util.rest.UriHelper;
 import de.shop.util.interceptor.Log;
-import de.shop.bestellverwaltung.service.BestellungService;
+import de.shop.util.rest.UriHelper;
 
+
+/**
+ * @author <a href="mailto:Juergen.Zimmermann@HS-Karlsruhe.de">J&uuml;rgen Zimmermann</a>
+ */
 @Path("/bestellungen")
-@Produces({ APPLICATION_JSON, APPLICATION_XML + ";qs=0.75", TEXT_XML + ";qs=0.5" })
+@Produces({APPLICATION_JSON, APPLICATION_XML + ";qs=0.75", TEXT_XML + ";qs=0.5" })
 @Consumes
 @RequestScoped
+@Transactional
 @Log
 public class BestellungResource {
 	@Context
@@ -43,62 +61,215 @@ public class BestellungResource {
 	private BestellungService bs;
 	
 	@Inject
-	private UriHelper uriHelper;
+	private ArtikelService as;
 	
+	@Inject
+	private ArtikelResource artikelResource;
+
 	@Inject
 	private KundeResource kundeResource;
 	
+	@Inject
+	private UriHelper uriHelper;
+	
+	/**
+	 * Mit der URL /bestellungen/{id} eine Bestellung ermitteln
+	 * @param id ID der Bestellung
+	 * @return Objekt mit Bestelldaten, falls die ID vorhanden ist
+	 */
 	@GET
 	@Path("{id:[1-9][0-9]*}")
-	public Response findBestellungById(@PathParam("id")
-									   Long id) {
-		final Bestellung bestellung = bs.findBestellungById(id);
+	public Response findBestellungById(@PathParam("id") Long id) {
+		final Bestellung bestellung = bs.findBestellungById(id, NUR_BESTELLUNG);
+
+		// URIs innerhalb der gefundenen Bestellung anpassen
 		setStructuralLinks(bestellung, uriInfo);
 		
-		return Response.ok(bestellung)
-                       .links(getTransitionalLinks(bestellung, uriInfo))
-                       .build();
+		// Link-Header setzen
+		final Response response = Response.ok(bestellung)
+                                          .links(getTransitionalLinks(bestellung, uriInfo))
+                                          .build();
+		return response;
 	}
 	
 	public void setStructuralLinks(Bestellung bestellung, UriInfo uriInfo) {
+		// URI fuer Kunde setzen
 		final AbstractKunde kunde = bestellung.getKundenid();
 		if (kunde != null) {
 			final URI kundeUri = kundeResource.getUriKunde(bestellung.getKundenid(), uriInfo);
 			bestellung.setKundeUri(kundeUri);
 		}
+		
+		// URIs fuer Artikel in den Postenen setzen
+		final Collection<Posten> Postenen = bestellung.getPosten();
+		if (Postenen != null && !Postenen.isEmpty()) {
+			for (Posten bp : Postenen) {
+				final URI artikelUri = artikelResource.getUriArtikel(bp.getArtikel(), uriInfo);
+				bp.setArtikelUri(artikelUri);
+			}
+		}
 	}
 	
-	private Link[] getTransitionalLinks(Bestellung bestellung, UriInfo uriInfo) {
+	public Link[] getTransitionalLinks(Bestellung bestellung, UriInfo uriInfo) {
 		final Link self = Link.fromUri(getUriBestellung(bestellung, uriInfo))
                               .rel(SELF_LINK)
                               .build();
 		final Link add = Link.fromUri(uriHelper.getUri(BestellungResource.class, uriInfo))
-							 .rel(ADD_LINK)
-							 .build();
-		
-		return new Link[]{self, add };
+                             .rel(ADD_LINK)
+                             .build();
+		return new Link[] { self, add };
 	}
-	
+
 	public URI getUriBestellung(Bestellung bestellung, UriInfo uriInfo) {
 		return uriHelper.getUri(BestellungResource.class, "findBestellungById", bestellung.getBestellnummer(), uriInfo);
 	}
 	
-/*	@POST
-	@Consumes({APPLICATION_JSON, APPLICATION_XML, TEXT_XML })
+	/**
+	 * Mit der URL /bestellungen/{id}/lieferungen die Lieferung ermitteln
+	 * zu einer bestimmten Bestellung ermitteln
+	 * @param id ID der Bestellung
+	 * @return Objekt mit Lieferdaten, falls die ID vorhanden ist
+	 */
+	@GET
+	@Path("{id:[1-9][0-9]*}/lieferungen")
+	public Response findLieferungenByBestellungId(@PathParam("id") Long id) {
+		// Diese Methode ist bewusst NICHT implementiert, um zu zeigen,
+		// wie man Methodensignaturen an der Schnittstelle fuer andere
+		// Teammitglieder schon mal bereitstellt, indem einfach ein "Internal
+		// Server Error (500)" produziert wird.
+		// Die Kolleg/inn/en koennen nun weiterarbeiten, waehrend man selbst
+		// gerade keine Zeit hat, weil andere Aufgaben Vorrang haben.
+		
+		// TODO findLieferungenByBestellungId noch nicht implementiert
+		return Response.status(INTERNAL_SERVER_ERROR)
+			       .entity("findLieferungenByBestellungId: NOT YET IMPLEMENTED")
+			       .type(TEXT_PLAIN)
+			       .build();
+	}
+
+	
+	/**
+	 * Mit der URL /bestellungen/{id}/kunde den Kunden einer Bestellung ermitteln
+	 * @param id ID der Bestellung
+	 * @return Objekt mit Kundendaten, falls die ID vorhanden ist
+	 */
+	@GET
+	@Path("{id:[1-9][0-9]*}/kunde")
+	public Response findKundeByBestellungId(@PathParam("id") Long id) {
+		final AbstractKunde kunde = bs.findKundeById(id);
+		kundeResource.setStructuralLinks(kunde, uriInfo);
+		
+		// Link Header setzen
+		final Response response = Response.ok(kunde)
+                                          .links(kundeResource.getTransitionalLinks(kunde, uriInfo))
+                                          .build();
+		return response;
+	}
+
+	
+	/**
+	 * Mit der URL /bestellungen eine neue Bestellung anlegen
+	 * @param bestellung die neue Bestellung
+	 * @return Objekt mit Bestelldaten, falls die ID vorhanden ist
+	 */
+	@POST
+	@Consumes({ APPLICATION_JSON, APPLICATION_XML, TEXT_XML })
 	@Produces
 	public Response createBestellung(@Valid Bestellung bestellung) {
-		bestellung = bs.createBestellung(bestellung);
+		// TODO eingeloggter Kunde wird durch die URI im Attribut "kundeUri" emuliert
+		final String kundeUriStr = bestellung.getKundeUri().toString();
+		int startPos = kundeUriStr.lastIndexOf('/') + 1;
+		final String kundeIdStr = kundeUriStr.substring(startPos);
+		Long kundeId = null;
+		try {
+			kundeId = Long.valueOf(kundeIdStr);
+		}
+		catch (NumberFormatException e) {
+			kundeIdInvalid();
+		}
 		
-		return Response.created(getUriBestellung(bestellung, uriInfo))
-			           .build();
-	}*/
-	 
-	@PUT
-	@Consumes({APPLICATION_JSON, APPLICATION_XML, TEXT_XML })
-	@Produces
-	public void updateBestellung(@Valid Bestellung bestellung) {
-		bs.updateBestellung(bestellung);
+		// IDs der (persistenten) Artikel ermitteln
+		final Collection<Posten> Posten = bestellung.getPosten();
+		final List<Long> artikelIds = new ArrayList<>(Posten.size());
+		for (Posten bp : Posten) {
+			final URI artikelUri = bp.getArtikelUri();
+			if (artikelUri == null) {
+				continue;
+			}
+			final String artikelUriStr = artikelUri.toString();
+			startPos = artikelUriStr.lastIndexOf('/') + 1;
+			final String artikelIdStr = artikelUriStr.substring(startPos);
+			Long artikelId = null;
+			try {
+				artikelId = Long.valueOf(artikelIdStr);
+			}
+			catch (NumberFormatException e) {
+				// Ungueltige Artikel-ID: wird nicht beruecksichtigt
+				continue;
+			}
+			artikelIds.add(artikelId);
+		}
+		
+		if (artikelIds.isEmpty()) {
+			// keine einzige Artikel-ID als gueltige Zahl
+			artikelIdsInvalid();
+		}
+
+		final Collection<Artikel> gefundeneArtikel = as.findArtikelByIds(artikelIds);
+		
+		// Postenen haben URLs fuer persistente Artikel.
+		// Diese persistenten Artikel wurden in einem DB-Zugriff ermittelt (s.o.)
+		// Fuer jede Posten wird der Artikel passend zur Artikel-URL bzw. Artikel-ID gesetzt.
+		// Postenen mit nicht-gefundene Artikel werden eliminiert.
+		int i = 0;
+		final List<Posten> neuePosten = new ArrayList<Posten>();
+		for (Posten bp : Posten) {
+			// Artikel-ID der aktuellen Posten (s.o.):
+			// artikelIds haben gleiche Reihenfolge wie Postenen
+			final long artikelId = artikelIds.get(i++);
+			
+			// Wurde der Artikel beim DB-Zugriff gefunden?
+			for (Artikel artikel : gefundeneArtikel) {
+				if (artikel.getId().longValue() == artikelId) {
+					// Der Artikel wurde gefunden
+					bp.setArtikel(artikel);
+					neuePosten.add(bp);
+					break;					
+				}
+			}
+			// FIXME JDK 8 hat Lambda-Ausdruecke
+			//final Artikel artikel = gefundeneArtikel.stream()
+			//                                        .filter(a -> a.getId() == artikelId)
+			//                                        .findAny();
+			//if (artikel != null) {
+			//	bp.setArtikel(artikel);
+			//	neuePostenen.add(bp);				
+			//}
+		}
+		bestellung.setPosten(neuePosten);
+		
+		bestellung = bs.createBestellung(bestellung, kundeId);
+
+		final URI bestellungUri = getUriBestellung(bestellung, uriInfo);
+		return Response.created(bestellungUri).build();
 	}
 	
+	/**
+	 * @NotNull verletzen, um die entsprechende Meldung zu verursachen, weil keine einzige Artikel-ID
+	 *          eine gueltige Zahl war.
+	 * @return null
+	 */
+	@NotNull(message = "{bestellung.artikelIds.invalid}")
+	public List<Long> artikelIdsInvalid() {
+		return null;
+	}
 	
+	/**
+	 * @NotNull verletzen, um die entsprechende Meldung zu verursachen, weil die Kunde-Id ungueltig ist.
+	 * @return null
+	 */
+	@NotNull(message = "{bestellung.kunde.id.invalid}")
+	public Long kundeIdInvalid() {
+		return null;
+	}
 }
