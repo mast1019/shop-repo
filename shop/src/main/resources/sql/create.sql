dda5ae3df15f066418d275dac9a6e32124db3c0f
Matthias Brand <brma1028 (at) hs-karlsruhe.de>
2013-12-20 15:40:30 +0100

    Beziehungen angepasst, INSERT.sql an Klassen angepasst, unnötige Klassen gelöscht, nicht benutzte Validationmessages gelöscht

--- shop/src/main/resources/sql/create.sql
+++ shop/src/main/resources/sql/create.sql
@@ -7,13 +7,11 @@
 --
 -- hibernate_sequence
 --
--- DROP SEQUENCE hibernate_sequence;
--- CREATE SEQUENCE hibernate_sequence START WITH 5000;
+DROP SEQUENCE hibernate_sequence;
+CREATE SEQUENCE hibernate_sequence START WITH 5000;
 
 --
 -- Check-Constraints fuer Enums
 --
--- ALTER TABLE lieferung ADD CONSTRAINT check_transporttype CHECK (TransportType IN ('ST', 'SCH', 'L', 'W'));
--- ALTER TABLE kunde ADD CONSTRAINT check_geschlecht CHECK (geschlecht IN ('M', 'W'));
--- ALTER TABLE kunde ADD CONSTRAINT check_familienstand CHECK (familienstand IN ('L', 'VH', 'G', 'VW'));
--- ALTER TABLE kunde_hobby ADD CONSTRAINT check_hobby CHECK (hobby IN ('S', 'L', 'R'));
+ALTER TABLE lieferung ADD CONSTRAINT check_transporttype CHECK (TransportType IN ('ST', 'SCH', 'L', 'W'));
+ALTER TABLE kunde ADD CONSTRAINT check_geschlecht CHECK (geschlecht IN ('M', 'W'));
