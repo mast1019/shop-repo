0862cb3bf87d2ad093b78e17574e2c9685dec071
Sarah Ludwig <sarah.ludwig911@yahoo.de>
2014-01-03 14:23:14 +0100

    warunungen beim kunde weggemacht

--- shop/src/main/java/de/shop/kundenverwaltung/domain/AbstractKunde.java
+++ shop/src/main/java/de/shop/kundenverwaltung/domain/AbstractKunde.java
@@ -1,8 +1,6 @@
 package de.shop.kundenverwaltung.domain;
 
 import static de.shop.util.Constants.KEINE_ID;
-import static javax.persistence.CascadeType.PERSIST;
-import static javax.persistence.CascadeType.REMOVE;
 
 import java.net.URI;
 import java.util.ArrayList;
--- shop/src/main/java/de/shop/kundenverwaltung/domain/Adresse.java
+++ shop/src/main/java/de/shop/kundenverwaltung/domain/Adresse.java
@@ -15,7 +15,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Index;
-import javax.persistence.JoinColumn;
+
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.Valid;
