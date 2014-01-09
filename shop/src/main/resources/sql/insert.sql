3eec4a451509ae5ad6b1099a4e7028265cf72c85
Matthias Brand <brma1028 (at) hs-karlsruhe.de>
2014-01-09 12:00:36 +0100

    inserts angepasst abstractauditable in Posten eingefügt

--- shop/src/main/resources/sql/insert.sql
+++ shop/src/main/resources/sql/insert.sql
@@ -61,10 +61,10 @@ INSERT INTO posten (id, bestellung_fk, artikel_fk, anzahl, erzeugt, aktualisiert
 --
 -- lieferung
 --
-INSERT INTO lieferung (id, liefernr, transport_art, erzeugt, aktualisiert) VALUES (600,'20051005-001','ST','01.08.2006 00:00:00','01.08.2006 00:00:00');
-INSERT INTO lieferung (id, liefernr, transport_art, erzeugt, aktualisiert) VALUES (601,'20051005-002','SCH','02.08.2006 00:00:00','02.08.2006 00:00:00');
-INSERT INTO lieferung (id, liefernr, transport_art, erzeugt, aktualisiert) VALUES (602,'20051005-003','L','03.08.2006 00:00:00','03.08.2006 00:00:00');
-INSERT INTO lieferung (id, liefernr, transport_art, erzeugt, aktualisiert) VALUES (603,'20051008-001','W','04.08.2006 00:00:00','04.08.2006 00:00:00');
+INSERT INTO lieferung (id, transport_art, erzeugt, aktualisiert) VALUES (600,'ST','01.08.2006 00:00:00','01.08.2006 00:00:00');
+INSERT INTO lieferung (id, transport_art, erzeugt, aktualisiert) VALUES (601,'SCH','02.08.2006 00:00:00','02.08.2006 00:00:00');
+INSERT INTO lieferung (id, transport_art, erzeugt, aktualisiert) VALUES (602,'L','03.08.2006 00:00:00','03.08.2006 00:00:00');
+INSERT INTO lieferung (id, transport_art, erzeugt, aktualisiert) VALUES (603,'W','04.08.2006 00:00:00','04.08.2006 00:00:00');
 
 --
 -- bestellung_lieferung
